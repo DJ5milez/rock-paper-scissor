37b78a7e0f6ad9fed1357218b68e1a80ca4958bd
DJ5milez <145610302+DJ5milez@users.noreply.github.com>
2024-11-28 20:15:24 +0000

    attempts in javascript

--- script.js
+++ script.js
@@ -20,24 +20,66 @@ window.addEventListener('click', (e) => {
   }
 });
 
-let userChoice = " " ;
-    if ()
+// When button is pressed log ID
+  //userChoice = button selected 
+  document.getElementById("rockBtn").addEventListener("click", rock);
+  document.getElementById("paperBtn").addEventListener("click", paper);
+  document.getElementById("scissorsBtn").addEventListener("click", scissors);
 
 
+  function rock() {
+    console.log ("Rock")
+  }
+  function paper() {
+    console.log ("Paper")
+  }
+  function scissors() {
+    console.log ("Scissors")
+  }
+  //if user select button => user chose X
+  function userChoice () {
+    if (document.getElementById("rockBtn").addEventListener("click", rock))
+      userChoice = "rock";
+    else if (document.getElementById("paperBtn").addEventListener("click", paper))
+      userChoice = "paper";
+    else if (document.getElementById("scissorsBtn").addEventListener("click", scissors))
+      userChoice = "Scissors";
+
+    return "Player has chosen" + userChoice
+  }
+
+  console.log(userChoice());    
+  
+ // function choice () {
+   // if (document.getElementById("rockBtn").addEventListener("click", choice))
+    //  choice = "rock";
+
+     // else if (document.getElementById("paperBtn").addEventListener("click", choice))
+     //   choice = "paper";
+
+     // else if (document.getElementById("scissorsBtn").addEventListener("click", choice))
+       // choice = "scissors";
+
+
+      
+    //return "Player has chosen" + choice
+ //  }
+
+  //function choice () {
+    //if (document.getElementById("rockBtn"))
+      //choice = "rock";
+   // else if (document.getElementById("paperBtn"))
+      //choice = "paper";
+   // else if (document.getElementById("paperBtn"))
+    //  choice = "scissors";
+
+   /// return "Player has chosen " + choice
+
+///  }
+
+  //console.log(choice());
             // comp makes choice
-                let randomNumber = Math.floor(Math.random()*3)+1;
-                console.log(randomNumber)
-            
-        
-                let handResult = " " ;
-                    if (randomNumber == 1)
-                        handResult = "Rock";
-                    else if (randomNumber == 2)
-                        handResult = "Paper";
-                    else if (randomNumber == 3)
-                        handResult = "Scissors";
 
-                console.log(handResult);
 
                 function compChoice() {
                     let randomNumber = Math.floor(Math.random()*3)+1;
